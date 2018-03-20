fdcc55a0fe0da7bcfa3427f9c6d542eb2edbc507
Admin <admin@admins-macbook-pro.local>
2018-03-19 19:58:57 -0700

    Roll Dice App

--- Roll Dice/ViewController.swift
+++ Roll Dice/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImage: UIImageView!
+    
+    @IBOutlet weak var diceLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +25,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+   
+    @IBAction func rolledDice(_ sender: Any) {
+        
+        let min : UInt32 = 1
+        let max : UInt32 = 7
+        
+        let randomNumber = arc4random_uniform(max-min) + 1
+        diceLabel.text = String("You Rolled \(randomNumber)")
+        
+        diceImage.image = UIImage(named: "Dice\(randomNumber)")
+    }
+    
 
 
 }
